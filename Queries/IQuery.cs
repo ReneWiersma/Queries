4a73e76c6eb26fb0df50eaf9a5630f79390b994b
René Wiersma <rene.wiersma@noordhoff.nl>
2020-12-14 13:17:22 +0100

    Initial

---
+++ Queries/IQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace ReneWiersma.Queries
+{
+    public interface IQuery<TIn, TOut>
+    {
+        TOut Execute(TIn input);
+    }
+
+    public interface IQuery<TOut>
+    {
+        TOut Execute();
+    }
+}
