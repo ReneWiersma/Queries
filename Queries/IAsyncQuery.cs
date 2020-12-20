a8a8ae2e6d3e74940d795cd9cc297ac6e8ffb787
René Wiersma <rene.wiersma@noordhoff.nl>
2020-12-20 07:39:27 +0100

    Add IAsyncQuery

---
+++ Queries/IAsyncQuery.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace ReneWiersma.Queries
+{
+    public interface IAsyncQuery<TIn, TOut>
+    {
+        Task<TOut> Execute(TIn input);
+    }
+
+    public interface IAsyncQuery<TOut>
+    {
+        Task<TOut> Execute();
+    }
+}
